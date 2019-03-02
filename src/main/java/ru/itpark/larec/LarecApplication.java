64e584e3e024ef71c7a99d10d80e4a2a87006f19
protpolymer <nvsvkont@gmail.com>
2019-03-02 14:52:28 +0300

    git initialization

---
+++ src/main/java/ru/itpark/larec/LarecApplication.java
@@ -0,0 +1,13 @@
+package ru.itpark.larec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LarecApplication.class, args);
+    }
+
+}
